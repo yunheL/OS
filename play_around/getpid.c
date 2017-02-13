717e7421f74c801506a2917a6392b45e422965b3
yunheL <liu348@wisc.edu>
2017-02-12 23:36:17 -0600

    getpid

--- play_around/getpid.c
+++ play_around/getpid.c
@@ -4,10 +4,22 @@
 #include <stdlib.h>
 #include <assert.h>
 
+/* ran this coupled of times and observed that
+even though pid is different each tmie but ppid
+is always the same. Use "ps" command and found
+out that ppid always refer to the shell in this
+case*/
 int
 main(int argc, char* argv[])
 {
+	int i;
 	pid_t pid = getpid();
-	printf("pid is %d\n", pid);
+	pid_t ppid = getppid();
+
+	for(i=0; i<3; i++)
+	{
+		printf("pid is %d\n", pid);
+		printf("ppid is %d\n", ppid);
+	}
 	return 0;
 }
