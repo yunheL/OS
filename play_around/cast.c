2bd2609efe80572c6a0b68a0f1aa7dc4af739e4e
yunheL <liu348@wisc.edu>
2017-02-22 23:29:32 -0600

    cast.c

--- play_around/cast.c
+++ play_around/cast.c
@@ -3,10 +3,16 @@
 
 int main()
 {
-	int a = 67;
-	char test = (char)a;
+	//cast int into char
+	int C = 67;
+	char test_char = (char)C;
 
-	printf("test_char is %c\n", test);
+	//cast char into int
+	char big_c = 'C';
+	int test_int = (int)big_c;
+
+	printf("test_char is %c\n", test_char);
+	printf("test_int is %d\n", test_int);
 
 	return 0;
 }
