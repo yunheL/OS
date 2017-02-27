34ea82535555e27e2b9cd4952c76f75efb4fdaba
yunheL <liu348@wisc.edu>
2017-02-26 23:09:49 -0600

    strlen.c

---
+++ play_around/strlen.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/*
+ This snapshot of the code is a record of idea
+ and haven't be compiled yet. 
+
+ TODO
+ compile the code
+ add timing mechanism to compare the two different
+ algorithms
+*/
+
+int main()
+{
+	s = "hello";
+	t = 0;
+	l = strlen(l);
+
+	//a O(n^2) algorithm
+	for(int i = 0; i<strlen(s); i++)
+	{
+		t += s(i);
+	}
+
+	//a O(n) algorithm
+	for(int i = 0; i<l; i++)
+	{
+		t += s(i);
+	}
+
+}
